a2d8058702da07c2e1f2525f85bae3c4e5140c43
stepank <stepankk@gmail.com>
2011-03-19 20:17:55 +0300

    added client and server error types

--- pyws/errors.py
+++ pyws/errors.py
@@ -1,5 +1,8 @@
+ET_CLIENT = 0
+ET_SERVER = 1
+
 class Error(Exception):
-    pass
+    error_type = ET_SERVER
 
 class ProtocolError(Error):
     pass
@@ -16,6 +19,7 @@ class FunctionError(Error):
     pass
 
 class FunctionNotFound(FunctionError):
+    error_type = ET_CLIENT
     def __str__(self):
         return 'A function cannot be found: %s' % self.args[0]
 
