259b4ce11a2b0d9048cddcfa7c135d38533d4d3e
stepank <stepankk@gmail.com>
2011-03-19 23:32:46 +0300

    validation exceptions

--- pyws/errors.py
+++ pyws/errors.py
@@ -4,6 +4,9 @@ ET_SERVER = 1
 class Error(Exception):
     error_type = ET_SERVER
 
+class ClientErrorTypeMixin(object):
+    error_type = ET_CLIENT
+
 class ProtocolError(Error):
     pass
 
@@ -18,11 +21,21 @@ class BadProtocol(ProtocolError):
 class FunctionError(Error):
     pass
 
-class FunctionNotFound(FunctionError):
-    error_type = ET_CLIENT
+class FunctionNotFound(ClientErrorTypeMixin, FunctionError):
     def __str__(self):
         return 'A function cannot be found: %s' % self.args[0]
 
 class BadFunction(FunctionError):
     def __str__(self):
         return 'Bad function: %s' % self.args[0]
+
+class FieldError(ClientErrorTypeMixin, Error):
+    pass
+
+class MissingFieldValue(FieldError):
+    def __str__(self):
+        return 'The value of \'%s\' field value is missing.' % self.args[0]
+
+class WrongFieldValueType(FieldError):
+    def __str__(self):
+        return 'The value of \'%s\' field is of wrong type' % self.args[0]
