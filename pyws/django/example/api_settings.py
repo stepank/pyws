0abe7505673e7b6841c3a4b98ba5d18a6d5f18cc
stepank <stepankk@gmail.com>
2011-03-19 23:36:44 +0300

    added sample functions

--- pyws/django/example/api_settings.py
+++ pyws/django/example/api_settings.py
@@ -6,8 +6,9 @@ PROTOCOLS = {
 }
 
 from pyws.functions.managers import FixedFunctionManager
-from pyws.example import simple
+from pyws.example import simple, simple_integer_adapter, simple_float_adapter
 
 FUNCTION_MANAGERS = (
-    FixedFunctionManager(simple),
+    FixedFunctionManager(
+        simple, simple_integer_adapter, simple_float_adapter),
 )
--- pyws/example/__init__.py
+++ pyws/example/__init__.py
@@ -1,2 +1,21 @@
-def simple(s):
-    return s + ' world'
+from pyws.functions import SimpleFunctionAdapter
+from pyws.functions.args import String, Integer, Float
+
+def simple(p):
+    return p + ' world'
+
+def simple_integer(p):
+    return p * 2
+
+simple_integer_adapter = SimpleFunctionAdapter(
+    simple_integer,
+    return_type=Integer,
+    args=(('p', Integer), ),
+)
+
+simple_float_adapter = SimpleFunctionAdapter(
+    simple_integer,
+    name='simple_float',
+    return_type=Float,
+    args=(('p', Float), ),
+)
