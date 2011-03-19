c6090fc7112c992cf74b5a5f401395278f1326c2
stepank <stepankk@gmail.com>
2011-03-19 23:34:05 +0300

    refactored Function

--- pyws/functions/__init__.py
+++ pyws/functions/__init__.py
@@ -1,34 +1,23 @@
-from args import Field, Record, String
-
-def getter(name):
-    def wrapped(self):
-        attr = getattr(self, name)
-        if callable(attr):
-            return attr()
-        return attr
-    return wrapped
-
-
-class FunctionInfo(object):
-
-    def __init__(self, return_type, args):
-        self.return_type = return_type
-        self.args = args
+from args import Field, Dict, String
 
 
 class Function(object):
 
+    def __init__(self, name, return_type=None, args=None):
+        self.name = name
+        self.return_type = return_type or String
+        if isinstance(args, Dict):
+            self.args = args
+        else:
+            self.args = Dict(name.capitalize() + 'Function',
+                *(Field(*field_args) for field_args in args))
+
     def __call__(self, **args):
-
         args = self.validate(args)
-
         return self.call(**args)
 
-    name = property(getter('_name'))
-    info = property(getter('_info'))
-
     def validate(self, args):
-        return self.info.args.validate(args)
+        return self.args.validate(args)
 
     def call(self, **args):
         raise NotImplemented('Function.call')
@@ -36,15 +25,13 @@ class Function(object):
 
 class SimpleFunctionAdapter(Function):
 
-    def __init__(self, origin):
+    def __init__(self, origin, name=None, return_type=None, args=None):
         self.origin = origin
+        super(SimpleFunctionAdapter, self).__init__(
+            name=name or origin.__name__,
+            return_type=return_type,
+            args=args or ((name, String) for name in self.origin.func_code.co_varnames)
+        )
 
     def call(self, **args):
         return self.origin(**args)
-
-    def _name(self):
-        return self.origin.__name__
-
-    def _info(self):
-        return FunctionInfo(String, Record(self.name.capitalize() + 'Function',
-            *(Field(name, String) for name in self.origin.func_code.co_varnames)))
