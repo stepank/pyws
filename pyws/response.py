f85b554dc9005431e7b5c870dac8560d203085ec
stepank <stepankk@gmail.com>
2011-05-15 21:29:32 +0400

    added content type property to response

--- pyws/response.py
+++ pyws/response.py
@@ -1,4 +1,5 @@
 class Response(object):
 
-    def __init__(self, text):
+    def __init__(self, text, content_type='text/plain'):
         self.text = text
+        self.content_type = content_type
