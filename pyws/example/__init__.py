1de5cb5c67185ac60f7ac9deba19efe839027652
stepank <stepankk@gmail.com>
2011-05-19 22:59:15 +0400

    added example functions with complext type arguments

--- pyws/example/__init__.py
+++ pyws/example/__init__.py
@@ -1,5 +1,5 @@
 from pyws.functions import NativeFunctionAdapter
-from pyws.functions.args import String, Integer, Float
+from pyws.functions.args import String, Integer, Float, DictOf, Field, ListOf
 
 def simple(p):
     return p + ' world'
@@ -9,6 +9,14 @@ def simple_integer(p):
 
 def add_integer(p, q):
     return p + q
+
+def add_dict(p, q):
+    return {'a': p['a'] + q['a'], 'b': p['b'] + q['b']}
+
+def add_list(p, q):
+    s, b = len(p) < len(q) and p, q or q, p
+    s.extend([0] * (len(b) - len(s)))
+    return list(s[i] + b[i] for i in xrange(len(s)))
 
 simple_integer_adapter = NativeFunctionAdapter(
     simple_integer,
@@ -41,3 +49,29 @@ add_float_adapter = NativeFunctionAdapter(
         ('q', Float),
     ),
 )
+
+ABDict = DictOf('ABDict',
+    Field('a', Integer),
+    Field('b', Integer),
+)
+
+add_dict_adapter = NativeFunctionAdapter(
+    add_dict,
+    return_type=ABDict,
+    args=(
+        ('p', ABDict),
+        ('q', ABDict),
+    ),
+)
+
+ListOfInteger = ListOf(Integer)
+
+add_list_adapter = NativeFunctionAdapter(
+    add_list,
+    return_type=ListOfInteger,
+    args=(
+        ('p', ListOfInteger),
+        ('q', ListOfInteger),
+    ),
+)
+
