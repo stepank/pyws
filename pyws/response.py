3c24a9f9e542ae8c19636c812cdbfdc5450848e2
stepank <stepankk@gmail.com>
2011-05-25 01:21:00 +0400

    added __str__ to response

--- pyws/response.py
+++ pyws/response.py
@@ -3,3 +3,16 @@ class Response(object):
     def __init__(self, text, content_type='text/plain'):
         self.text = text
         self.content_type = content_type
+
+    def __str__(self):
+        return """<pyws.response.Response
+    content_type: %s
+    text:
+%s
+>""" % (
+            self.content_type,
+            self.text.strip()
+        )
+
+    def __unicode__(self):
+        return unicode(self.__str__())
