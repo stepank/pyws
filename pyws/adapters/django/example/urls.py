ece17a6edbe97a4bc0bca8eae016f4e1a7515605
stepank <stepank@stepank-laptop.(none)>
2011-08-24 19:09:48 +0400

    fixed raw_post_data crash in test form

--- pyws/adapters/django/example/urls.py
+++ pyws/adapters/django/example/urls.py
@@ -38,7 +38,7 @@ def test_form(request):
         fake_request = HttpRequest()
         fake_request.GET = urlparse.parse_qs(query_string)
         if not query_string:
-            fake_request.raw_post_data = request_text
+            fake_request._raw_post_data = request_text
 
         response = serve(fake_request, protocol + '/' + tail, server)
 
