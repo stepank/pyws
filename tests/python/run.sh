b747f6dccb4d18499878299bc6fb1d68f2f696f7
stepank <stepankk@gmail.com>
2011-10-10 21:55:07 +0400

    python test runner accepts test file name

--- tests/python/run.sh
+++ tests/python/run.sh
@@ -2,4 +2,10 @@
 
 BASE_DIR=`dirname $(readlink -f $0)`
 
-unit2 discover -s $BASE_DIR/testcases/ -p *.py
+if [ "$1" = "" ]; then
+    PATTERN=*
+else
+    PATTERN=$1
+fi;
+
+unit2 discover -s $BASE_DIR/testcases/ -p $PATTERN.py
