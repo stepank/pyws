a1e8ec78a6bfe73b8ade7e5f788c85e309adee43
stepank <stepankk@gmail.com>
2011-10-23 23:28:14 +0400

    docstest runner script

---
+++ tests/run_doctests.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+import os, sys
+sys.path.append(os.path.abspath(os.path.dirname(__file__) + '/../src/'))
+
+import doctest
+
+modules = [
+    'pyws.functions.register',
+    'pyws.functions.args.types',
+    'pyws.functions.args.types.complex',
+]
+
+if __name__ == '__main__':
+    for module in modules:
+        __import__(module)
+        doctest.testmod(sys.modules[module])
