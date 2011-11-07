1d13be3b71a2f713940fcb1267b63e0ee3c35baf
stepank <stepankk@gmail.com>
2011-11-07 21:56:56 +0400

    now version is stored in pyws/__init__.py

--- setup.py
+++ setup.py
@@ -1,26 +1,26 @@
 #!/usr/bin/env python
 
+import sys, os
+sys.path.append(
+    os.path.join(os.path.dirname(os.path.abspath(__file__)), 'src'))
+
 from setuptools import setup
 from setuptools import find_packages
 
-try:
-    #noinspection PyUnresolvedReferences
-    import lxml
-except ImportError:
-    print 'error: lxml is required'
-    exit()
+import pyws
 
 setup(
     name='pyws',
-    version='dev',
+    version=pyws.VERSION,
     package_dir={'': 'src'},
     packages=find_packages('src'),
     description='Python SOAP server providing WSDL',
     long_description='Python SOAP server providing WSDL',
-    keywords=['soap', 'wsdl', 'server'],
+    keywords=['soap', 'wsdl', 'server', 'xml', 'json'],
     author='Stepan N. Kornyakov',
     author_email='stepankk@gmail.com',
     url='https://github.com/stepank/pyws',
     license='MIT',
     platforms=['Any'],
+    install_requires=['lxml'],
 )
