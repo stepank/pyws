eacc2ba5418ae8346f5c29f974e5aff1ae52d616
stepank <stepankk@gmail.com>
2013-01-20 19:33:44 +0400

    some minor refactoring of the suds tests

--- tests/python/suds/factory.py
+++
@@ -1,7 +0,0 @@
-import os
-import suds
-
-wsdl_path = os.path.abspath(os.path.dirname(__file__)) + '/test.wsdl'
-
-def build_client():
-    return suds.client.Client('file://%s' % wsdl_path, cache=None)
--- tests/python/suds/testcases/base.py
+++ tests/python/suds/testcases/base.py
@@ -1,12 +1,17 @@
-from factory import build_client
+import os
+
+import suds
+
 from rest_client import RestClient
+
+wsdl_path = os.path.realpath(os.path.join(
+    os.path.abspath(os.path.dirname(__file__)), '..', 'test.wsdl'))
 
 
 class BaseTestCaseMixin(object):
 
     def setUp(self):
-        client = build_client()
-        self.client = client
-        self.service = client.service
-        self.factory = client.factory
+        self.client = suds.client.Client('file://%s' % wsdl_path, cache=None)
+        self.service = self.client.service
+        self.factory = self.client.factory
         self.rest_client = RestClient()
